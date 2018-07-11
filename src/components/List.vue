c2fede8c52dffac134259b3ac888438a25b89177
Trikolon <1077860+Trikolon@users.noreply.github.com>
2018-07-11 14:05:39 +0200

    created component to display api data

---
+++ src/components/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>List of {{type}}</h1>
+    {{data}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    type: {
+      required: true,
+      type: String,
+    },
+    data: {
+      required: true,
+      validator(data) {
+        return Array.isArray(data);
+      },
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
-  </v-container>
+  <List v-if="data" :data="data" type="Bans"></List>
 </template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      data: null,
+    };
+  },
+};
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>

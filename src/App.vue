cfd32019b22968c4bed6d1f940905b9e9af49b12
Metype <39036543+metype@users.noreply.github.com>
2020-03-29 00:34:56 -0500

    I'm trying

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,58 @@
 <template>
-    <div>
-        <v-app :dark="darkTheme">
-            <v-toolbar color="primary" dark fixed clipped-left app>
-                <router-link to="/">
-                    <v-toolbar-title id="title"  class="mr-3" >
-                        {{$appName}}
-                    </v-toolbar-title>
-                </router-link>
-                <!-- FIXME: mobile view -->
-                <v-text-field
-                        flat
-                        solo-inverted
-                        hide-details
-                        prepend-inner-icon="search"
-                        label="Player Lookup"
-                        v-model="playerInput"
-                        @change="playerLookup"
-                ></v-text-field>
-                <v-spacer></v-spacer>
-                <v-toolbar-items>
-                    <v-btn href="https://forum.tallcraft.com/new-topic?category_id=5" flat>Ban Dispute</v-btn>
-                </v-toolbar-items>
-            </v-toolbar>
-            <v-content>
-                <v-container fluid>
-                    <router-view></router-view>
-                </v-container>
-
-
-            </v-content>
-            <v-footer>
-                <v-spacer></v-spacer>
-                <v-btn flat icon @click="darkTheme = !darkTheme">
-                    <v-icon>invert_colors</v-icon>
-                </v-btn>
-                <v-spacer></v-spacer>
-            </v-footer>
-        </v-app>
-    </div>
+  <div>
+    <v-app :dark="darkTheme">
+      <v-toolbar color="primary" dark fixed clipped-left app>
+        <router-link to="/">
+          <v-toolbar-title id="title" class="mr-3">{{$appName}}</v-toolbar-title>
+        </router-link>
+        <!-- FIXME: mobile view -->
+        <v-text-field
+          flat
+          solo-inverted
+          hide-details
+          prepend-inner-icon="search"
+          label="Player Lookup"
+          v-model="playerInput"
+          @change="playerLookup"
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-btn href="https://forum.tallcraft.com/new-topic?category_id=5" flat>Ban Dispute</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <v-content>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+      <v-footer>
+        <v-spacer></v-spacer>
+        <v-btn flat icon @click="darkTheme = !darkTheme">
+          <v-icon>invert_colors</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+      </v-footer>
+    </v-app>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
-      playerInput: '',
-      darkTheme: false,
+      playerInput: "",
+      darkTheme: false
     };
   },
   watch: {
     $route() {
-      this.playerInput = '';
+      this.playerInput = "";
     },
     // Save darkTheme state in localStorage
     darkTheme() {
-      if (localStorage) localStorage.setItem('darkTheme', this.darkTheme);
-    },
+      if (localStorage) localStorage.setItem("darkTheme", this.darkTheme);
+    }
   },
   beforeMount() {
     this.darkTheme = this.getThemeState();
@@ -71,18 +66,18 @@ export default {
         return false;
       }
       try {
-        return JSON.parse(localStorage.getItem('darkTheme') || false);
+        return JSON.parse(localStorage.getItem("darkTheme") || false);
       } catch (error) {
         return false;
       }
-    },
-  },
+    }
+  }
 };
 </script>
 
 
 <style scoped>
-    #title{
-        color: white !important;
-    }
+#title {
+  color: white !important;
+}
 </style>
